713a4b369b802d47399361d32666633a1e0d1821
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2023-01-21 12:10:37 -0500

    will

--- GameJamJan21/Assets/Scripts/BulletFire.cs
+++ GameJamJan21/Assets/Scripts/BulletFire.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BulletFire : MonoBehaviour
 {
+    private Rigidbody rb;
+    public int maxBounces = 6;
+    
     public float rotationSpeed = 0.3f;
     public float bulletSpeed = 3f;
     // 0 = Not fired
@@ -13,12 +16,12 @@ public class BulletFire : MonoBehaviour
     private Vector3 motion;
     private Vector3 m_EulerAngleVelocity;
 
-    private Rigidbody rb;
-
     // Start is called before the first frame update
     void Start()
     {
         m_EulerAngleVelocity = new Vector3(0, 100, 0);   
+        rb = GetComponent <Rigidbody> ();
+        // rb.velocity = Vector3.back;
     }
 
     // Update is called once per frame
@@ -45,7 +48,35 @@ public class BulletFire : MonoBehaviour
     }
 
     void InFlightBulletMove() {
-        print(transform.forward);
+        // print(transform.forward);
         transform.position += transform.forward * Time.deltaTime * bulletSpeed;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("collided with something");
+        // Check tag for Transient or Reflector
+        // Reflect if applicable
+        if (collision.gameObject.tag == "Transient")
+        {
+            // Do nothing and pass through
+            Physics.IgnoreCollision(GetComponent<Collider>(), collision.gameObject.GetComponent<Collider>());
+            // Call the add score function
+        } else  {
+            // Ricochet
+            ContactPoint contact = collision.contacts[0];
+            Vector3 oldvel = rb.velocity;
+            float speed = oldvel.magnitude;
+            
+            rb.velocity = Vector3.Reflect(oldvel.normalized, contact.normal);
+            print("Old velocity: " + oldvel.ToString() + " Old speed: " + speed.ToString() + " New vel: " + rb.velocity.ToString() + " New speed: " + rb.velocity.magnitude.ToString());
+
+            // Subtract bounces and maybe destroy
+            maxBounces -= 1;
+            if (maxBounces < 1) {
+                Destroy(gameObject);
+            }
+        }
+
     }
 }
