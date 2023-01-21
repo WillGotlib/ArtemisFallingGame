aef85ec774105193ba44dbe80bfdc85569396799
Kevin Portinga <kdports@gmail.com>
2023-01-21 18:04:14 -0500

    Somewhat improved

--- GameJamJan21/Assets/Scripts/PlayerController.cs
+++ GameJamJan21/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private bool aiming_status = false;
     public GameObject projectile;
     private GameObject bullet;
+    float turnSmoothVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +25,21 @@ public class PlayerController : MonoBehaviour
             float vertical = Input.GetAxisRaw("Vertical");
             Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
+            int sensitivity = 3;
+            float turnSmoothTime = 0.1f;
+            
+            float rotateHorizontal = Input.GetAxis("Mouse X");
+            float rotateVertical = Input.GetAxis("Mouse Y");
+            transform.Rotate(transform.up * rotateHorizontal * sensitivity);
+            // transform.Rotate(transform.right * rotateVertical);
+
             if (direction.magnitude >= 0.1f) {
-                controller.Move(direction * speed * Time.deltaTime);
+                float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
             }
 
             if (Input.GetMouseButtonDown(1)) {
