9bd73d74a84ab0291fda8741a1354b07943a49a5
Kevin Portinga <kdports@gmail.com>
2023-01-21 16:49:56 -0500

    Simple movement

--- GameJamJan21/Assets/Scripts/PlayerController.cs
+++ GameJamJan21/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController controller;
+    public float speed = 6f;
+    private bool aiming_status = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (aiming_status == 0) {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+            if (direction.magnitude >= 0.1f) {
+                controller.Move(direction * speed * Time.deltaTime);
+            }
+
+            if (Input.GetMouseButtonDown(1)) {
+                startAiming();
+            }
+                
+        } else {
+            if (Input.GetMouseButtonDown(1))
+                aiming_status = false;
+        }
+    }
+
+    void startAiming() {
+        aiming_status = true;
     }
 }
