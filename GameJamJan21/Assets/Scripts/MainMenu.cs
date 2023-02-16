7d7566ed92053416e1f42d421a430f82c8621f5d
woom221 <sean.yi@mail.utoronto.ca>
2023-02-16 13:43:20 -0500

    fixed a minor bug
    now when return to menu and press play again, the game properly unpauses

--- GameJamJan21/Assets/Scripts/MainMenu.cs
+++ GameJamJan21/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame() {
+        if (PausedMenu.isPaused == true) {
+            Time.timeScale = 1f;
+            PausedMenu.isPaused = false;
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
