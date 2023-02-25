637e48e9075cd8fa329ed560bfe9405724e8b874
zivoy <ziv132@gmail.com>
2023-02-25 13:57:47 -0500

    fixed bugs in install script

--- Scripts/installDeps.sh
+++ Scripts/installDeps.sh
@@ -7,25 +7,22 @@ then
 fi
 
 download (){
-  curl -L -o "package.zip" $1
+  curl -L -o "package.zip" "$1"
+  mkdir -p "./Assets/$2"
   unzip "package.zip" -d "./Assets/$2" > /dev/null
   rm -f "package.zip"
 }
 
 downloadNuget (){
-  download "https://www.nuget.org/api/v2/package/$1/$2" "Plugins/$1
-"
+  download "https://www.nuget.org/api/v2/package/$1/$2" "Plugins/$1"
   cd Assets/Plugins
-  mv "$1/lib/net462" .
+  mv "$1/lib/$3" .
   rm -rf "$1"
   mkdir -p "$1/lib"
   mv "$3" "$1/lib/"
   cd ../..
-  echo "installed channels"
 }
 
-downloadNuget Google.Protobuf 3.22.0 net45
-echo "installed protobuf"
+downloadNuget Google.Protobuf 3.22.0 net45 && echo "installed protobuf"
 
-downloadNuget System.Runtime.CompilerServices.Unsafe 4.5.2 netstandard2.0
-echo "installed system.unsafe"
+downloadNuget System.Runtime.CompilerServices.Unsafe 4.5.2 netstandard2.0 && echo "installed system.unsafe"
