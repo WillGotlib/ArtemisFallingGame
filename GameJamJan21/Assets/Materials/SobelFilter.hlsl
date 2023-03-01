b7da06ea4973709e18408420ae826d805326f4a7
zivoy <ziv132@gmail.com>
2023-02-28 23:38:33 -0500

    tried making sobel 2d

--- GameJamJan21/Assets/Materials/SobelFilter.hlsl
+++ GameJamJan21/Assets/Materials/SobelFilter.hlsl
@@ -2,41 +2,48 @@
 #ifndef MYHLSLINCLUDE_INCLUDED
 #define MYHLSLINCLUDE_INCLUDED
 
-float3x3 kernel_x1=float3x3(-1, 0, 1,
-                            -2, 0, 2,
-                            -1, 0, 1);
-float3x3 kernel_x2=float3x3(-2, 0, 2,
-                            -4, 0, 4,
-                            -2, 0, 2);
-float3x3 kernel_x3=float3x3(-1, 0, 1,
-                            -2, 0, 2,
-                            -1, 0, 1);
+float2x2 kernel_x1=float2x2(-1, 1,
+                            -1, 1);
+float2x2 kernel_x2=float2x2(-1, 1,
+                            -1, 1);
 
-float3x3 kernel_y1=float3x3(-1,-2,-1,
-                             0, 0, 0,
-                             1, 2, 1);
-float3x3 kernel_y2=float3x3(-2,-4,-2,
-                             0, 0, 0,
-                             2, 4, 2);
-float3x3 kernel_y3=float3x3(-1,-2,-1,
-                             0, 0, 0,
-                             1, 2, 1);
+float2x2 kernel_y1=float2x2(-1,-1,
+                             1, 1);
+float2x2 kernel_y2=float2x2(-1,-1,
+                             1, 1);
 
-float3x3 kernel_z1=float3x3(-1,-2,-1,
-                            -2,-4,-2,
-                            -1,-2,-1);
-float3x3 kernel_z2=float3x3( 0, 0, 0,
-                             0, 0, 0,
-                             0, 0, 0);
-float3x3 kernel_z3=float3x3( 1, 2, 1,
-                             2, 4, 2,
-                             1, 2, 1);
+float2x2 kernel_z1=float2x2(-1,-1,
+                            -1,-1);
+float2x2 kernel_z2=float2x2( 1, 1,
+                             1, 1);
 
-void Sobel_float(const float3 colour, out float3 Out)
+void Sobel_float(const float colour, out float3 Out)
 {
-    float x = mul(colour, kernel_x1) * mul(colour, kernel_x2) * mul(colour, kernel_x3);
-    float y = mul(colour, kernel_y1) * mul(colour, kernel_y2) * mul(colour, kernel_y3);
-    float z = mul(colour, kernel_z1) * mul(colour, kernel_z2) * mul(colour, kernel_z3);
+    float4 input;
+    input.x = colour;
+
+    const float dx = ddx(input.x);
+    const float dy = ddy(input.x);
+    input.y = input.x - dx;
+    input.z = input.x - dy;
+
+    const float didx = ddx(input.y); 
+    const float didy = ddy(input.z);
+    const float tempx = input.y - didx;
+    const float tempy = input.z - didy;
+
+    if(abs(input.x)-tempx<0.001)
+    {
+        input.w = tempx;
+    }
+    else
+    {
+        input.w = tempy;
+    }
+    
+    float x = mul(input, kernel_x1) * mul(input, kernel_x2);
+    float y = mul(input, kernel_y1) * mul(input, kernel_y2);
+    float z = mul(input, kernel_z1) * mul(input, kernel_z2);
     Out = float3(x,y,z);
 }
 
