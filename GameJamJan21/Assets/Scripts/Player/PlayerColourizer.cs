7a95d816c9272600579f03eba31ba78748d3fe67
zivoy <ziv132@gmail.com>
2023-03-16 00:26:52 -0400

    fixes for doing init after colourizing

--- GameJamJan21/Assets/Scripts/Player/PlayerColourizer.cs
+++ GameJamJan21/Assets/Scripts/Player/PlayerColourizer.cs
@@ -8,20 +8,28 @@ public class PlayerColourizer : MonoBehaviour
     [SerializeField] private Material secondary;
     [SerializeField] private string colourAttribute = "_Colour";
 
-    private Color _primaryColour;
-    private Color _secondaryColour;
+    private Color _primaryColour = Color.clear;
+    private Color _secondaryColour = Color.clear;
 
     public Color PrimaryColour
     {
         get => _primaryColour;
-        set => _primaryMat?.SetColor(_colourAttributeId, _primaryColour = value);
+        set
+        {
+            _primaryColour = value;
+            if (_primaryMat) _primaryMat.SetColor(_colourAttributeId, value);
+        }
     }
 
     public Color SecondaryColour
     {
         get => _secondaryColour;
-        set => _secondaryMat?.SetColor(_colourAttributeId, _secondaryColour = value);
-}
+        set
+        {
+            _secondaryColour = value;
+            if (_secondaryMat) _secondaryMat.SetColor(_colourAttributeId, value);
+        }
+    }
 
     private Material _primaryMat;
     private Material _secondaryMat;
@@ -29,6 +37,8 @@ public class PlayerColourizer : MonoBehaviour
 
     public void initialColourize()
     {
+        if (_primaryMat != null) return; // dont init if already inited
+        
         _colourAttributeId = Shader.PropertyToID(colourAttribute);
         _primaryMat = Instantiate(primary);
         _secondaryMat = Instantiate(secondary);
@@ -57,8 +67,14 @@ public class PlayerColourizer : MonoBehaviour
             renderer.materials = materials;
         }
 
+        if (_primaryColour != Color.clear)
+            PrimaryColour = _primaryColour;
+        else
+            _primaryColour = primary.GetColor(_colourAttributeId);
         
-        _primaryColour = primary.GetColor(_colourAttributeId);
-        _secondaryColour = secondary.GetColor(_colourAttributeId);
+        if (_secondaryColour != Color.clear)
+            SecondaryColour = _secondaryColour;
+        else 
+            _secondaryColour = secondary.GetColor(_colourAttributeId);
     }
 }
