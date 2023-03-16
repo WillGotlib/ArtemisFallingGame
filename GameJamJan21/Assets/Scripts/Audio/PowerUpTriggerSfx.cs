a1fe03bf84448387feeebbe3ee72772e10018d60
essigg <vikramadityas.chouhan@gmail.com>
2023-03-16 08:56:38 -0400

    Audio Fixes for Ubi_build, Added dash and powerupsfx

--- GameJamJan21/Assets/Scripts/Audio/PowerUpTriggerSfx.cs
+++ GameJamJan21/Assets/Scripts/Audio/PowerUpTriggerSfx.cs
@@ -7,7 +7,7 @@ public class PowerUpTriggerSfx : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FMODUnity.RuntimeManager.PlayOneShot("event:/Actions/PowerUppickup", GetComponent<Transform>().position);   
+           
     }
 
     // Update is called once per frame
